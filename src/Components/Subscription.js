754f1699e565cc7ec4c825343d712ba3b1042d86
mohan_2231 <mahan.singh.tomar1034@gmail.com>
2024-03-24 19:16:07 +0530

    refine design 1

--- src/Components/Subscription.js
+++ src/Components/Subscription.js
@@ -7,14 +7,14 @@ const Subscription = () => {
             <div className='sm:w-[50%]'>
 
                 <p className='font-bold  text-[#12406F]  text-xl' >Subscribe</p>
-                <p className='py-3'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
-                <div className="flex flex-col sm:flex-row items-center">
+                <p className='py-3 sm:w-[70%]'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna</p>
+                <div className="flex flex-col sm:flex-row gap-4 items-center mb-8 sm:mr-10 mt-3">
                     <input
                         type="text"
-                        className="w-full sm:w-auto sm:px-4 py-2  border border-gray-300 rounded mb-4 sm:mb-0 sm:py-2"
+                        className="w-full sm:px-4 py-2  border border-gray-300 rounded sm:mb-0 sm:py-2"
                         placeholder="Your email address"
                     />
-                    <button className="bg-[#12406F] text-white p-2 rounded-lg px-4 ml-0 sm:ml-2 mt-2 sm:mt-0">
+                    <button className="bg-[#12406F] text-white p-2 rounded-lg px-4 ml-0 sm:ml-2 sm:mt-0 whitespace-nowrap">
                         Subscribe Now
                     </button>
                 </div>
